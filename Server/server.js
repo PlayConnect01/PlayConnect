1765769663ef0dcef5981fdd693fd3277dd1dff2
eyasahbeni <eyasahbeni23707@gmail.com>
2025-01-01 21:33:09 +0100

    im pulingg from the  main  good jobb fro now

--- Server/server.js
+++ Server/server.js
@@ -22,7 +22,8 @@ const competetionRouter = require('./routes/competetion');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const favorites = require('./routes/favoriteRoutes');
-const paymentRouter = require('./routes/Paymentrouter.js');
+const paymentRoutes
+ = require('./routes/Paymentrouter.js');
 const notificationRoutes = require('./routes/notification');
 const orderRoutes = require('./routes/orderRoutes');
 const orderHistoryRoutes = require('./routes/orderHistoryRoutes');
@@ -82,7 +83,7 @@ app.use('/password', passwordRouter);
 app.use('/product', productRoutes);
 app.use('/cart', cartRoutes);
 app.use('/favorites', favorites);
-app.use('/payments', paymentRouter);
+app.use('/payments', paymentRoutes);
 app.use('/leaderboard', leaderboardRoutes);
 app.use('/chats', chatRoutes);
 app.use('/orderHistory', orderHistoryRoutes);
